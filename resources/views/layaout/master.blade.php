a9be42c824ec73fa1cc2633a35bfaf1e158e99b6
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2023-01-26 09:43:35 -0600

    cambie iconos del master

--- resources/views/layaout/master.blade.php
+++ resources/views/layaout/master.blade.php
@@ -7,7 +7,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <title>
-        UTC
+        ARARAT
     </title>
     <!-- Token de VUE -->
     <!-- TOKEN -->
@@ -37,7 +37,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
                 <img src="assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold text-white">UTC</span>
+                <span class="ms-1 font-weight-bold text-white">ARARAT</span>
             </a>
         </div>
         <hr class="horizontal light mt-0 mb-2">
@@ -52,35 +52,35 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white " href="../pages/tables.html">
+                    <a class="nav-link text-white " href="productos">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-icons opacity-10">table_view</i>
+                            <i class="material-icons opacity-10">shopping_bag</i>
                         </div>
-                        <span class="nav-link-text ms-1">Grupos</span>
+                        <span class="nav-link-text ms-1">Productos</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white " href="../pages/billing.html">
+                    <a class="nav-link text-white " href="categorias">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">receipt_long</i>
                         </div>
-                        <span class="nav-link-text ms-1">Maestros</span>
+                        <span class="nav-link-text ms-1">Categorias</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white " href="../pages/virtual-reality.html">
+                    <a class="nav-link text-white " href="tipoDeProd">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">view_in_ar</i>
                         </div>
-                        <span class="nav-link-text ms-1">Alumnos</span>
+                        <span class="nav-link-text ms-1">Tipo de Productos</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white " href="../pages/rtl.html">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
+                            <i class="material-icons opacity-10">monetization_on</i>
                         </div>
-                        <span class="nav-link-text ms-1">RTL</span>
+                        <span class="nav-link-text ms-1">Ventas</span>
                     </a>
                 </li>
                 <li class="nav-item">
